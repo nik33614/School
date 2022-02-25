27de0ccc65933e3ab5896d2474b5efa2dae7416c
Nikita <58114134+nik33614@users.noreply.github.com>
2022-02-25 22:23:12 +0300

    Update 112166.py

--- 112166.py
+++ 112166.py
@@ -1,7 +1,6 @@
 from math import*
 x,y = map(float,input().split())
-if(x>=0) and (x<=pi) and (y<=sin(x)) and (y<=0.5) and (y>=0):#(y>=0)and(y<0,5)and(y<sin(x))and(x>0)and(x<=3)and(x<=pi)
+if(x>=0) and (x<=pi) and (y<=sin(x)) and (y<=0.5) and (y>=0):
     print("YES")
 else:
     print("NO")
-#(y>=0)and(y<0,5)and(y<sin(x))and(x>0)and(x<=3)and(x<=pi)
